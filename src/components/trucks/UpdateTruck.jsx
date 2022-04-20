5f695095fca70a66ea9973627dbb57582b90be40
jordan <jwald3@brockport.edu>
2022-04-20 10:30:59 -0400

    Addressed style errors on web view

--- src/components/trucks/UpdateTruck.jsx
+++ src/components/trucks/UpdateTruck.jsx
@@ -1,61 +1,82 @@
-import React, { useState, useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
-import TruckService from '../../services/TruckService';
+import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import TruckService from "../../services/TruckService";
 
 const UpdateTruck = () => {
-    let navigate = useNavigate();
-    let { id } = useParams();
-    const [license, setLicense] = useState("");
-    const [capacity, setCapacity] = useState("");
+  let navigate = useNavigate();
+  let { id } = useParams();
+  const [license, setLicense] = useState("");
+  const [capacity, setCapacity] = useState("");
 
-    useEffect(() => {
-        TruckService.getTruckById(id).then(res => {
-            let truck = res.data;
-            setLicense(truck.license);
-            setCapacity(truck.capacity);
-        })
-    }, [])
+  useEffect(() => {
+    TruckService.getTruckById(id).then((res) => {
+      let truck = res.data;
+      setLicense(truck.license);
+      setCapacity(truck.capacity);
+    });
+  }, []);
 
-    const updateTruck = (e) => {
-        e.preventDefault();
+  const updateTruck = (e) => {
+    e.preventDefault();
 
-        let truck = {
-            "license": license, 
-            "capacity": capacity
-        }
+    let truck = {
+      license: license,
+      capacity: capacity,
+    };
 
-        TruckService.updateTruck(id, truck).then(_res => {
-            navigate("/trucks/");
-        });
-    }
+    TruckService.updateTruck(id, truck).then((_res) => {
+      navigate("/trucks/");
+    });
+  };
 
-    return (
-        <div className="truck-update-container">
-            <div className="truck-update-sub-container">
-                <div className="heading">
-                    <h2>Update Truck</h2>
-                </div>
-                <div className="truck-update-form">
-                    <form onSubmit={updateTruck}>
-                        <div className='form-group'>
-                            <label>License Plate:</label>
-                            <input required placeholder='Truck License Plate' name="license" className="form-control" 
-                                value={license} onChange={(event) => setLicense(event.target.value)}/>
-                        </div>
-                        <div className='form-group'>
-                            <label>Truck Capacity:</label>
-                            <input required placeholder='Truck Capacity' name="capacity" className="form-control" 
-                                value={capacity} onChange={(event) => setCapacity(event.target.value)}/>
-                        </div>
-                        <div className="update-truck-button-row">
-                            <button type="submit" className=''>Save</button>
-                            <button type="button" className='' style={{marginLeft: "10px"}} onClick={() => navigate("/trucks/")}>Cancel</button>
-                        </div>
-                  </form>
-                </div>
-            </div>
+  return (
+    <div className="truck-update-container">
+      <div className="truck-update-sub-container">
+        <div className="truck-heading">
+          <h2>Update Truck</h2>
         </div>
-    )
-}
+        <div className="truck-update-form">
+          <form onSubmit={updateTruck}>
+            <div className="form-group">
+              <label>License Plate:</label>
+              <input
+                required
+                placeholder="Truck License Plate"
+                name="license"
+                className="form-control"
+                value={license}
+                onChange={(event) => setLicense(event.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <label>Truck Capacity:</label>
+              <input
+                required
+                placeholder="Truck Capacity"
+                name="capacity"
+                className="form-control"
+                value={capacity}
+                onChange={(event) => setCapacity(event.target.value)}
+              />
+            </div>
+            <div className="update-truck-button-row">
+              <button type="submit" className="btn btn-success">
+                Save
+              </button>
+              <button
+                type="button"
+                className="btn btn-secondary"
+                style={{ marginLeft: "10px" }}
+                onClick={() => navigate("/trucks/")}
+              >
+                Cancel
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default UpdateTruck;
